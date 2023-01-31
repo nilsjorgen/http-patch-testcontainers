3f01a35804716adb5ce31da1a3680f7757ee8ad4
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2023-01-31 10:23:55 +0100

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.6.4"
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
-    kotlin("plugin.spring") version "1.6.10"
-    kotlin("jvm") version "1.6.10"
+    id("org.springframework.boot") version "3.0.2"
+    id("io.spring.dependency-management") version "1.1.0"
+    id("org.jlleitschuh.gradle.ktlint") version "11.1.0"
+    kotlin("plugin.spring") version "1.8.0"
+    kotlin("jvm") version "1.8.0"
 }
 
 repositories {
@@ -13,17 +13,16 @@ repositories {
 }
 
 dependencies {
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-    implementation(platform("org.testcontainers:testcontainers-bom:1.16.3"))
-    implementation(kotlin("stdlib-jdk8"))
-    implementation(kotlin("reflect"))
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.squareup.okhttp3:okhttp:4.9.3")
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+//    implementation(kotlin("stdlib"))
+//    implementation(kotlin("reflect"))
 
+    testImplementation(platform("org.testcontainers:testcontainers-bom:1.17.6"))
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.testcontainers:testcontainers")
     testImplementation("org.testcontainers:junit-jupiter")
-    testImplementation("org.amshove.kluent:kluent:1.68")
+    testImplementation("org.amshove.kluent:kluent:1.72")
 }
 
 tasks.withType<KotlinCompile> {
